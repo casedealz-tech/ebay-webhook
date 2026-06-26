5e67ee95048731cd16b524cfc2b869c957b10f91
casedealz-tech <casedealz@gmail.com>
2026-06-26 11:28:30 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+app.use(express.json());
+
+app.get('/ebay-deletion', (req, res) => {
+  const challengeCode = req.query.challenge_code;
+  if (!challengeCode) return res.status(400).send('No challenge code');
+  
+  const endpoint = 'https://YOUR-APP-NAME.onrender.com/ebay-deletion';
+  const verificationToken = 'bargz_ebay_token_2024';
+  
+  const hash = crypto.createHash('sha256')
+    .update(challengeCode + verificationToken + endpoint)
+    .digest('hex');
+  
+  res.json({ challengeResponse: hash });
+});
+
+app.post('/ebay-deletion', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Running'));
